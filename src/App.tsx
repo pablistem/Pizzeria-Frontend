c9823ce9435cfdd2599443e4ddfa85e24cec63f1
Leandro Medina <medinaleandron@gmail.com>
2023-11-01 21:30:33 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
- const App:React.FC = ()=>{
- 
-return (
+const App: React.FC = () => {
+  return (
     <>
       <h1>Pizzería Don Remolo</h1>
       <p>hola</p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
