55b6af0ac302cdc0e407946f0e411aae17d48f89
Leandro Medina <medinaleandron@gmail.com>
2023-11-03 03:40:22 -0300

    revert file changes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,21 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-        singleQuote: true,
-      },
-    ],
-    quotes: ['error', 'single'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
