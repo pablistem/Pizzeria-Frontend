66d1480127c90ff42b3f114dd4e8682c4237ca0c
Leandro Medina <medinaleandron@gmail.com>
2023-11-03 04:09:15 -0300

    Add plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,11 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:react/jsx-runtime',
-    'eslint-config-prettier',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
