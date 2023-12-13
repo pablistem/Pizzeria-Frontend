e41db4b3d3b3e159f3e29319c9ea149ee2778c81
nacho <jose-imhoff@hotmail.com>
2023-12-13 20:31:54 -0300

    Fix problems with navbar test

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -13,6 +13,7 @@ export const Navbar = () => {
 
         <label
           htmlFor="menu"
+          id="label-menu"
           className="bg-open-menu w-6 h-5 bg-cover bg-center cursor-pointer peer-checked:bg-close-menu transition-all delay-300 z-50 max-w-screen-lg: ml-10"
         ></label>
         <h1>Pizzeria Don Remolo</h1>
