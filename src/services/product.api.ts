7db7951ea34701b54d57d77df2efb96b7c8a8212
Axel Ledesma <74274367+axel-ledesma-david@users.noreply.github.com>
2024-01-26 18:35:31 -0300

    fix import and test

--- src/services/product.api.ts
+++ src/services/product.api.ts
@@ -1,6 +1,6 @@
 import { AxiosResponse } from 'axios';
 import { IProduct } from '../types/types';
-import Axios from '../services/axios.api';
+import { Axios } from '../services/axios.api';
 
 export const getProductsData = async (): Promise<IProduct[]> => {
   const res: AxiosResponse<IProduct[]> = await Axios.get('/api/products');
