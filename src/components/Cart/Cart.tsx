d3aed572ed556d2def4d1de9327ff166ff4bab44
Axel Ledesma <74274367+axel-ledesma-david@users.noreply.github.com>
2024-03-05 19:05:14 -0300

    fix types and corrections

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import Modal from '../Modal/Modal';
-import ModalIcon from '../../assets/Modal icons/icons8-flecha-derecha-30.png';
+import OrderModal from '../Modal/OrderModal';
 
 const Cart = () => {
   const [modal, setModal] = useState(false);
+
+  const closeModal = () => setModal(!modal);
 
   const elementos = [
     {
@@ -135,68 +137,7 @@ const Cart = () => {
       </div>
       {modal ? (
         <Modal>
-          <div className="justify-center w-[100%] flex flex-col gap-4">
-            <b className="text-base mr-7">Detalles de la entrega</b>
-            <div className="flex-col flex gap-7 justify-center w-[100%]">
-              <div className="flex justify-between items-center ml-3">
-                <div>
-                  <b>Calle random 123</b>
-                  <p>Dirección</p>
-                </div>
-                <img
-                  src={ModalIcon}
-                  alt="Modal Icon"
-                  className="w-6 h-6 mr-3"
-                />
-              </div>
-              <div className="flex justify-between items-center ml-3">
-                <div>
-                  <b>****123 | Mastercard</b>
-                  <p>Forma de pago</p>
-                </div>
-                <img
-                  src={ModalIcon}
-                  alt="Modal Icon"
-                  className="w-6 h-6 mr-3"
-                />
-              </div>
-              <div className="flex justify-between items-center ml-3">
-                <div>
-                  <b> -$500</b>
-                  <p>Cupón</p>
-                </div>
-                <img
-                  src={ModalIcon}
-                  alt="Modal Icon"
-                  className="w-6 h-6 mr-3"
-                />
-              </div>
-            </div>
-            <b>Resumen de tu pedido</b>
-            <div className="w-[100%] flex flex-col gap-3">
-              <div className="flex justify-around w-[100%]">
-                <p>Subtotal</p>
-                <span className="font-bold">$9100</span>
-              </div>
-              <div className="flex justify-around w-[100%]">
-                <p>Descuento</p>
-                <span className="font-bold"> -$500</span>
-              </div>
-              <div className="flex justify-around w-[100%]">
-                <b>Total</b>
-                <b>$9400</b>
-              </div>
-            </div>
-            <div className="justify-center items-center flex w-[100%]">
-              <button
-                type="button"
-                className="w-[90%] rounded-full bg-logo-orange text-white p-1"
-                onClick={() => setModal(false)}
-              >
-                Pedir ($4000)
-              </button>
-            </div>
-          </div>
+          <OrderModal closeModal={() => closeModal()} />
         </Modal>
       ) : null}
     </div>
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,17 +1,13 @@
-import PropTypes from 'prop-types';
+import { ReactNode } from 'react';
 
-const Modal = ({ children }) => {
+const Modal = ({ children }: { children: ReactNode }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center">
-      <div className="bg-bg-profile py-5 px-2 roundend flex flex-col justify-center items-center gap-5 w-[40vw] h-[80vh] font-sans shadow-sm">
+      <div className="bg-bg-profile py-5 px-2 roundend flex flex-col justify-center items-center gap-5 lg:w-[50vw] md:w-[60vw] w-[80%] h-[80vh] font-sans shadow-sm">
         {children}
       </div>
     </div>
   );
-};
-
-Modal.propTypes = {
-  children: PropTypes.node.isRequired,
 };
 
 export default Modal;
