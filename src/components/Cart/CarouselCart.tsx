4b8c44f972fb86d7c0f69614afe69f4bc5458d00
nacho <jose-imhoff@hotmail.com>
2024-03-13 19:07:50 -0300

    Add hook useProduct

---
+++ src/components/Cart/CarouselCart.tsx
@@ -0,0 +1,32 @@
+import { useCart } from '../../hooks/useCart';
+import useProducts from '../../hooks/useProduct';
+const CarouselCart = () => {
+  const { productsData } = useProducts();
+  const { addToCart } = useCart();
+  return (
+    <div className="bg-white p-4 mb-20 h-40 w-[90%] flex justify-center items-center self-center rounded-xl overflow-x-auto overflow-y-auto space-x-4 relative bottom-5">
+      <div className="flex space-x-3">
+        {productsData?.map((product, i) => (
+          <div
+            key={i}
+            className="flex-none justify-center items-center bg-white self-center"
+          >
+            <img
+              src={product.image}
+              alt="imagen"
+              className="w-32 h-32 object-cover object-center"
+            />
+            <button
+              className="bg-logo-orange rounded-3xl text-xs text-text-paragraph w-full items-center self-center"
+              onClick={() => addToCart(product)}
+            >
+              Agregar al carrito
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CarouselCart;
